3f7187a01c97555e731912656d342512f2a7fdc8
arunvishwas <arunvishwas96@gmail.com>
2023-10-12 09:56:36 +0000

    change

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountID=1456326;
+let accountEmail="arunvishwas96@gmail.com";
+var accountPassword="arun9690";
+accountCit="Bareilly";
+
+let accountNumber;
+ 
+accountName = "Mohit Mistri";
+
+
+console.table([accountID,accountEmail,accountPassword,accountCit,accountNumber,accountName]);
+ 
+
