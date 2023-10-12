f6523830b192dd1066e905daa8f937616194fda4
arunvishwas <arunvishwas96@gmail.com>
2023-10-12 09:46:37 +0000

    changes

--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,13 @@
-console.log("arun vishwas");
-console.log("hai mera ek accha dost");
+const accountID=1456326;
+let accountEmail="arunvishwas96@gmail.com";
+var accountPassword="arun9690";
+accountCit="Bareilly";
+
+let accountNumber;
+ 
+accountName = "Mohit Mistri";
+
+
+console.table([accountID,accountEmail,accountPassword,accountCit,accountNumber,accountName]);
+ 
+
