dbda19cec460454ae0e37591f0d317ca5651c728
arunvishwas <arunvishwas96@gmail.com>
2023-10-12 12:09:23 +0000

    change

---
+++ basics/datatype.js
@@ -0,0 +1,19 @@
+/*primitive data types*/
+//number
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value(empty value represntation) - the datatype is object 
+//ndefined = value not give to a variable
+//symbol = unique
+
+ //object
+
+ //type of use for known the data type of any string or value
+ 
+//  let a = 32;
+
+//  console.log(typeof null); out-object
+//  console.log(typeof undefined); out-undefine
+//  console.log(typeof a); out-number
+
