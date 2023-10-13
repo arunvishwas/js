8654ad099faafac2e0f53ab3ccaa373fee951b39
arunvishwas <arunvishwas96@gmail.com>
2023-10-13 06:39:38 +0000

    change

---
+++ basics/conoperation.js
@@ -0,0 +1,19 @@
+//How to change value in negative by - marks
+let i = 44;
+let NegValue = -i;
+console.log(typeof NegValue);
+console.log(NegValue);
+
+
+//concatenate of two vlaues
+let str1="arun";
+let str2="Vishwas";
+let str3=str1 +" " + str2;
+console.log(str3);
+
+//modulus or remainder operator
+
+let val1=3;
+let val2=2;
+let val3=val1%val2;
+console.log(val3);
