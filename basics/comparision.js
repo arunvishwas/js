f1c91aaf07a6ef0b4e442ffbac4d9e46efc0dd41
arunvishwas <arunvishwas96@gmail.com>
2023-10-13 09:54:46 +0000

    change

--- basics/comparision.js
+++ basics/comparision.js
@@ -8,3 +8,9 @@ console.log(2!=1);
  console.log(null>0);
  console.log(null==0);
  console.log(null>=0);
+ console.log(undefined==0);
+ console.log(undefined>0);
+ console.log(undefined<0);
+
+ // == double equal only check value === triple equal to check both datatype and value also
+   
---
+++ basics/primitivedatatype.js
@@ -0,0 +1,31 @@
+//primitive datatype: only give the copy of data not the memory address
+//7 types: string, Number, Boolean, null, undefined, symbol, Bigint
+
+//reference type (non primitive)
+
+//Array, Objects, Functions
+
+//Javascript 
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+const score = 100;
+const scoreVal=100.1;
+const isloggedIn=false;
+const outsidetemp=null;
+let userEmail = undefined; //you can also define it manually
+const id=Symbol('123');
+const anotherId=Symbol('123');
+//two symbol value never be same
+
+
+// array and object
+const heros =["shaktiman","naagraj","doga"];
+//object in always {} it have key value pair
+let myObj ={
+name:"hitesh",
+age:22,
+
+}
+
+
+ 
