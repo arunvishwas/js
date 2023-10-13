61da719dc027a0cdfc63a2722a18d71b5c83cd84
arunvishwas <arunvishwas96@gmail.com>
2023-10-13 08:47:03 +0000

    change

--- basics/conoperation.js
+++ basics/conoperation.js
@@ -17,3 +17,13 @@ let val1=3;
 let val2=2;
 let val3=val1%val2;
 console.log(val3);
+
+//concatenate in console.log
+
+console.log(1+2); // out in number 3
+console.log(typeof "1"+2); // out in string 2
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+2+"3"); //out  in stirng first two number add than 3 will be concatenate out-33
+//boolean
+console.log()
---
+++ basics/postandpre.js
@@ -0,0 +1,9 @@
+//postfix operator
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+//prefix
+let gameCounterpre=100;
+ ++gameCounterpre;
+console.log(gameCounterpre);
