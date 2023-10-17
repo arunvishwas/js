4fe5b2eb4f648a423aec534be596ab1651ac84af
arunvishwas <arunvishwas96@gmail.com>
2023-10-17 10:37:54 +0000

    changes

---
+++ object/object02.js
@@ -0,0 +1,20 @@
+//change the object value
+const obj ={
+
+name:"arun",
+age:8,
+email:"arunvishwas96@gmail.com",
+address:"puranpur pilibhit",
+
+
+}
+
+
+
+//change value 
+console.log(obj.name="Keshav");
+console.log(obj.name);
+
+//if you want stop the changes
+Object.freeze(obj); //original array will we same their happen no changes
+
