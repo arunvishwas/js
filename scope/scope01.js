b3fb6f324c1481a2bdef4e3bc690a63642774880
arunvishwas <arunvishwas96@gmail.com>
2023-10-21 06:55:07 +0000

    changes

--- scope/scope01.js
+++ scope/scope01.js
@@ -27,8 +27,52 @@ function two(){
 
 }
 
+two();
+
+}
+if(true){
+const user = "hitesh";
+if(user==="hitesh")
+{
+const website = "youtub";
+console.log(user+website);
 
 
 }
 
+//console.log(website) // we don't access the websit here because the scope 
+//is block level scope
 
+
+}
+
+//console.log() //the scope of a variable in if condition in between the curly bracket block level access
+
+
+
+//+++++++++++++++intersting concept ++++++++++++++
+
+function addone(num)
+{
+ return num+1;
+}
+
+const varr = addone(2);
+console.log(varr);
+
+
+
+//declare a function by variable 
+
+const addtwo = function(num2){
+
+
+return num2+5;
+
+
+}
+
+console.log(addtwo(5)); // access 
+
+
+console.log(addtwo(6)+2) //access
