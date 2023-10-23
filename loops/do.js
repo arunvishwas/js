d44a1860d937945889a6aba99a3af7461f16f14e
arunvishwas <arunvishwas96@gmail.com>
2023-10-23 09:26:12 +0000

    changes

---
+++ loops/do.js
@@ -0,0 +1,34 @@
+
+ 
+// while(condition){
+// }
+
+
+let index =0;
+while(index<=10){
+
+    console.log(`value is index ${index}`);
+    index++;
+}
+
+let i =0;
+
+let arr1=["batman","superman","joker","aquaman"]
+
+while(i<arr1.length){
+
+console.log(`value in array are ${arr1[i]}`)
+i++;
+
+}
+
+let score =11;
+do {
+
+console.log("I will print definatley");
+score++;
+}while(score<10){
+
+console.log(`value`);
+
+}
--- loops/loops02.js
+++ loops/loops02.js
@@ -7,3 +7,24 @@ for(let i=0;i<arr1.length;i++){
 
 //break and continue
 
+for(let i=1;i<=20;i++){
+
+if(i==5){
+
+  console.log(`Detected 5`);
+  break;  
+}
+console.log(`value of i is ${i}`);
+
+}
+
+//continue it skip one condition
+for(let i=0;i<20;i++){
+
+if(i==5){
+console.log(`detected 5`);
+continue; //it skip the i=5 condition an then program will run further
+
+}
+console.log(`${i}`);
+}
